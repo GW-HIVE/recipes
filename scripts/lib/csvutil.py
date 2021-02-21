8747fe751dcbe2c1115f412b8c007bfab4ce9692
Ned Cauley <escauley@gwmail.gwu.edu>
2021-02-21 12:49:39 -0500

    Added base files from oncomx and glygen servers

---
+++ scripts/lib/csvutil.py
@@ -0,0 +1,462 @@
+import csv
+import json
+
+
+def split_col(df_list, table_ind, action_obj):
+
+    field_list = df_list[table_ind]["fields"]
+    data_frame = {"fields":field_list, "data":df_list[table_ind]["data"]}
+   
+    field_idx = field_list.index(action_obj["field"])
+    delim = action_obj["delim"]
+    new_field_list = field_list[0:field_idx] + action_obj["newfields"] 
+    new_field_list += field_list[field_idx+1:]
+
+    row_list = []
+    for row in data_frame["data"]:
+        val_list = row[field_idx]
+        newrow = row[0:field_idx] + row[field_idx].split(delim) + row[field_idx+1:]
+        row_list.append(newrow)
+    
+    data_frame["data"] = row_list
+    data_frame["fields"] = new_field_list
+
+    return data_frame
+
+
+
+def add_average_col(df_list, table_ind, action_obj):
+
+    field_list = df_list[table_ind]["fields"]
+    data_frame = {"fields":field_list, "data":df_list[table_ind]["data"]}
+    
+    row_grps = {}
+    for row in data_frame["data"]:
+        key_list = []
+        for f in action_obj["anchorfields"]:
+            key_list.append(row[field_list.index(f)])
+        group_key = " ".join(key_list)
+        if group_key not in row_grps:
+            row_grps[group_key] = []
+        row_grps[group_key].append(row)
+
+    row_list = []
+    for group_key in row_grps:
+        x_count, x_sum, x_max,x_min = 0.0, 0.0, -100000.0, 1000000.0
+        for row in row_grps[group_key]:
+            x = float(row[field_list.index(action_obj["field"])])
+            x_sum += x
+            x_count += 1.0
+        x_average = "%.5f" % (x_sum/x_count)
+        for row in row_grps[group_key]:
+            newrow =  row + [x_average]
+            row_list.append(newrow)
+    
+    data_frame["data"] = row_list
+    data_frame["fields"] = field_list + [action_obj["newfield"]]
+    return data_frame
+
+
+
+    
+
+def add_normalized_col(df_list, table_ind, action_obj):
+
+    field_list = df_list[table_ind]["fields"]
+    data_frame = {"fields":field_list, "data":df_list[table_ind]["data"]}
+    
+    row_grps = {}
+    for row in data_frame["data"]:
+        key_list = []
+        for f in action_obj["anchorfields"]:
+            key_list.append(row[field_list.index(f)])
+        group_key = " ".join(key_list)
+        if group_key not in row_grps:
+            row_grps[group_key] = []
+        row_grps[group_key].append(row)
+
+    row_list = []
+    for group_key in row_grps:
+        x_sum, x_max,x_min = 0.0, -100000.0, 1000000.0
+        for row in row_grps[group_key]:
+            x = float(row[field_list.index(action_obj["field"])])
+            x_max = x if x > x_max else x_max
+            x_min = x if x < x_min else x_min
+            x_sum += x
+
+        for row in row_grps[group_key]:
+            x = float(row[field_list.index(action_obj["field"])])
+            #x_normalized = (x-x_min)/(x_max-x_min)
+            x_normalized = "%.5f" % (x/x_sum)
+            newrow =  row + [x_normalized]
+            row_list.append(newrow)
+
+
+    data_frame["data"] = row_list
+    data_frame["fields"] = field_list + [action_obj["newfield"]]
+    return data_frame
+
+
+
+def transpose_cols(df_list, table_ind, action_obj):
+    
+    field_list = df_list[table_ind]["fields"]
+    data_frame = {"fields":field_list, "data":df_list[table_ind]["data"]}
+    start_col_idx = action_obj["startcolidx"]
+    new_field_one = action_obj["newfieldone"]
+    new_field_two = action_obj["newfieldtwo"]
+
+    row_list = []
+    field_list_new = field_list[0:start_col_idx] + [new_field_one, new_field_two]
+    for row in data_frame["data"]:
+        for j in xrange(start_col_idx, len(row)):
+            v = row[j]
+            newrow = row[0:start_col_idx] + [field_list[j],v]
+            row_list.append(newrow)
+
+    data_frame["data"] = row_list
+    data_frame["fields"] = field_list_new
+
+    return data_frame
+
+
+def expand_rows(df_list, table_ind, action_obj):
+
+    field_list = df_list[table_ind]["fields"]
+    data_frame = {"fields":field_list, "data":df_list[table_ind]["data"]}
+    expansion_field = action_obj["expansionfield"]
+    expansion_delim = action_obj["expansiondelim"]
+    row_list = []
+    for row in data_frame["data"]:
+        field_idx = field_list.index(expansion_field)
+        val_list = list(set(row[field_idx].strip().split(expansion_delim)))
+        
+        for val in val_list:
+            tmprow = []
+            for j in xrange(0, len(row)):
+                tmprow.append(row[j] if j != field_idx else val)
+            row_list.append(tmprow)
+    
+    data_frame["data"] = row_list
+
+    return data_frame
+
+
+def filter_out_records(df_list, table_ind, action_obj):
+
+    field_list = df_list[table_ind]["fields"]
+    data_frame = {"fields":field_list, "data":df_list[table_ind]["data"]}
+
+    row_list = []
+    for row in data_frame["data"]:
+        flag_list = []
+        for obj in action_obj["conditionlist"]:
+            cond_field = obj["field"]
+            cond_value = obj["value"]
+            data_value = row[field_list.index(cond_field)]
+            if obj["operation"] == "in":
+                flag_list.append(data_value in cond_value)
+
+        flag_list_unique = list(set(flag_list))
+        if len(flag_list) == len(action_obj["conditionlist"]) and flag_list_unique == [True]:
+            continue
+        row_list.append(row)
+
+    data_frame["data"] = row_list
+    return data_frame
+
+
+def filter_in_records(df_list, table_ind, action_obj):
+
+    field_list = df_list[table_ind]["fields"]
+    data_frame = {"fields":field_list, "data":df_list[table_ind]["data"]}
+
+    row_list = []
+    for row in data_frame["data"]:
+        flag_list = []
+        for obj in action_obj["conditionlist"]:
+            cond_field = obj["field"]
+            cond_value = obj["value"]
+            data_value = row[field_list.index(cond_field)]
+            if obj["operation"] == "in":
+                flag_list.append(data_value in cond_value)
+
+        flag_list_unique = list(set(flag_list))
+        if len(flag_list) == len(action_obj["conditionlist"]) and flag_list_unique == [True]:
+            row_list.append(row)
+
+    data_frame["data"] = row_list
+    return data_frame
+
+
+
+def add_combo_field(df_list, table_ind, action_obj):
+
+    field_list = df_list[table_ind]["fields"] + [action_obj["combofield"]]
+    data_frame = {"fields":field_list, "data":df_list[table_ind]["data"]}
+   
+
+    for row in data_frame["data"]:
+        extra_row = []
+        for f in action_obj["fieldlist"]:
+            extra_row.append(row[field_list.index(f)].replace("\"", "").strip())
+        row += [action_obj["merge_char"].join(extra_row)]
+
+    return data_frame
+
+
+def add_constant_fields(df_list, table_ind, new_data):
+
+    new_data_field_list = new_data.keys()
+    field_list = df_list[table_ind]["fields"] + new_data_field_list
+    data_frame = {"fields":field_list, "data":df_list[table_ind]["data"]}
+
+    extra_row = []
+    for f in new_data_field_list:
+        extra_row.append(new_data[f])
+    for row in data_frame["data"]:
+        row += extra_row
+
+    return data_frame
+
+
+
+
+
+def union_tables(df_list,ind_list):
+
+    out_tbl = []
+    first_ind = ind_list[0] - 1
+    for i in ind_list:
+        ind = i - 1
+        out_tbl += df_list[ind]["data"]
+    
+    seen = {}
+    data_frame = {"fields":df_list[first_ind]["fields"], "data":[]}
+    for row in out_tbl:
+        row_str = json.dumps(row)
+        if row_str in seen:
+            continue
+        data_frame["data"].append(row)
+        seen[row_str] = True
+
+    return data_frame
+
+
+def load_large_sheet(sheet_obj, in_file, field_list, separator):
+
+    import sys
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
+    import io
+
+    sheet_obj["fields"] = []
+    sheet_obj["data"] = []
+    seen = {}
+    field_ind_list = []
+    
+    #with io.open(in_file, "r") as FR:
+    with io.open(in_file, "r", encoding="utf-8-sig",errors="ignore") as FR:
+        rowcount = 0
+        ncols = 0
+        for line in FR:
+            row = line.strip().split(separator)
+            #row[0] = row[0].split("\"")[1]
+            row[0] = row[0].replace("\"", "")
+            row[-1] = row[-1].replace("\"", "")
+            rowcount += 1
+            if rowcount == 1:
+                for j in xrange(0, len(row)):
+                    row[j] = row[j].strip().replace("\"", "")
+                bad_fields = []
+                for f in field_list:
+                    if f not in row:
+                        bad_fields.append(f)
+                if bad_fields != []:
+                    print "input file:",  in_file
+                    print "fields in file:", row
+                    print "fields for extraction:", field_list
+                    print "non matching fields: ", bad_fields
+                    sys.exit()
+
+                
+                #capture number of columns here
+                ncols = len(row)
+                field_list = row if field_list == [] else field_list
+                for f in field_list:
+                    field_index = row.index(f)
+                    if field_index != -1:
+                        sheet_obj["fields"].append(f)
+                        field_ind_list.append(field_index)
+            else:
+                #make sure every row has ncols columns
+                if len(row) != ncols:
+                    print "bad row %s" % (rowcount)
+                    print row
+                    sys.exit()
+                new_row = []
+                for j in field_ind_list:
+                    new_row.append(row[j].strip())
+                if json.dumps(new_row) not in seen:
+                    sheet_obj["data"].append(new_row)
+                    seen[json.dumps(new_row)] = True
+    return
+
+
+def load_sheet_as_dict(sheet_obj, in_file, separator, anchor_field):
+
+
+    seen = {}
+    
+    if "fields" not in sheet_obj:
+        sheet_obj["fields"] = []
+    if "data" not in sheet_obj:
+        sheet_obj["data"] = {}
+
+
+    f_list = []
+    with open(in_file, 'r') as FR:
+        csv_grid = csv.reader(FR, delimiter=separator, quotechar='\"')
+        row_count = 0
+        for row in csv_grid:
+            if json.dumps(row) in seen:
+                continue
+            seen[json.dumps(row)] = True
+            row_count += 1
+            if row_count == 1:
+                f_list = row
+                for j in xrange(0, len(row)):
+                    if row[j] == anchor_field:
+                        continue
+                    sheet_obj["fields"].append(row[j].strip().replace("\"", ""))
+            else:
+                new_row = []
+                for j in xrange(0, len(row)):
+                    if f_list[j] == anchor_field:
+                        continue
+                    new_row.append(row[j].strip().replace("\"", "`"))
+                main_id = row[f_list.index(anchor_field)]
+                if main_id not in sheet_obj["data"]:
+                    sheet_obj["data"][main_id] = [] 
+                sheet_obj["data"][main_id].append(new_row)
+    return
+
+
+def load_sheet_from_json(sheet_obj, in_file, field_list):
+
+    sheet_obj["fields"] = field_list
+    sheet_obj["data"] = []
+    doc = json.loads(open(in_file, "r").read())
+    for obj in doc:
+        val_dict = {}
+        for prop_lineage in field_list:
+            val = obj
+            for prop in prop_lineage.split("."):
+                if prop in val:
+                    val = val[prop]
+                else:
+                    val = ""
+            val_dict[prop_lineage] = val
+        row = []
+        for f in field_list:
+            val = str(val_dict[f]) if f in val_dict else ""
+            row.append(val)
+        sheet_obj["data"].append(row)
+    
+    return
+
+def load_sheet(sheet_obj, in_file, field_list, separator):
+
+    seen = {}
+    sheet_obj["fields"] = []
+    sheet_obj["data"] = []
+    field_ind_list = []
+    with open(in_file, 'r') as FR:
+        csv_grid = csv.reader(FR, delimiter=",", quotechar='\"')
+        row_count = 0
+        ncols = 0
+        for row in csv_grid:
+            if json.dumps(row) in seen:
+                continue
+            seen[json.dumps(row)] = True
+            row_count += 1
+            for j in xrange(0, len(row)):
+                row[j] = row[j].replace("\"", "`")
+            if row_count == 1:
+                ncols = len(row)
+                for j in xrange(0, len(row)):
+                    f = row[j].strip()
+                    if f in field_list:
+                        field_ind_list.append(j)
+                        sheet_obj["fields"].append(f)
+            else:
+                #make sure every row has ncols columns
+                if len(row) != ncols:
+                    continue
+                new_row = []
+                for j in field_ind_list:
+                    new_row.append(row[j].strip())
+                sheet_obj["data"].append(new_row)
+
+    
+    
+    return
+
+
+def load_workbook(workbook_obj, fileset_objlist, separator):
+
+    for obj in fileset_objlist:
+        for file_name in obj["filenamelist"]:
+            in_file = obj["dir"] + file_name
+            workbook_obj["sheets"][file_name] = {}
+            load_sheet(workbook_obj["sheets"][file_name], in_file, ",")
+
+    return
+
+
+
+def join_tables(df_one, df_two, anchor_fields):
+    #It is assumed that the first column in both tables are the anchor columns
+    
+    df_three = {"fields":[], "data":[]}
+    df_three["fields"] = []
+    for f in df_one["fields"]:
+        df_three["fields"].append(f)
+
+    for f in df_two["fields"]:
+        if f != anchor_fields[1]:
+            df_three["fields"].append(f)
+
+    row_dict_two = {}
+    for row in df_two["data"]:
+        anchor_id = row[df_two["fields"].index(anchor_fields[1])]
+        if anchor_id not in row_dict_two:
+            row_dict_two[anchor_id] = []
+        newrow = []
+        for j in xrange(0, len(df_two["fields"])):
+            if df_two["fields"][j] != anchor_fields[1]:
+                newrow.append(row[j])
+        row_dict_two[anchor_id].append(newrow)
+
+    row_two_empty = []
+    for j in xrange(1, len(df_two["fields"])):
+        row_two_empty.append("")
+
+
+    for row_one in df_one["data"]:
+        anchor_id = row_one[df_one["fields"].index(anchor_fields[0])]
+        if anchor_id in row_dict_two:
+            for row_two in row_dict_two[anchor_id]:
+                df_three["data"].append(row_one + row_two)
+        else:
+            df_three["data"].append(row_one + row_two_empty)
+
+
+    return df_three
+
+
+
+
+
+
+
---
+++ scripts/make-dataset.py
@@ -0,0 +1,241 @@
+import os,sys
+import json
+import csv
+#import requests
+
+from optparse import OptionParser
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio import pairwise2
+from Bio.SubsMat import MatrixInfo as matlist
+from Bio.pairwise2 import format_alignment
+
+from Bio.SeqRecord import SeqRecord
+from Bio.Alphabet import IUPAC
+
+import commands
+import glob
+import datetime
+import pytz
+
+sys.path.append('../lib/')
+import csvutil
+
+
+__version__="1.0"
+__status__ = "Dev"
+
+def dump_log(log_file, msg, flag):
+
+    ts = datetime.datetime.now(pytz.timezone('US/Eastern')).strftime('%Y-%m-%d %H:%M:%S %Z%z')
+    if flag == "write":
+        with open(log_file, "w") as FW:
+            FW.write("%s\t%s\n" % (ts,msg))
+    else:
+        with open(log_file, "a") as FA:
+            FA.write("%s\t%s\n" % (ts, msg))
+
+    return
+
+
+def execute_recipe(dataset):
+
+    masterlist_file = "unreviewed/biomarker_masterlist.csv"
+    recipe_file = "recipes/%s.json" % (dataset)
+    log_file = "logs/%s.log" % (dataset)
+    
+    recipe_json = json.loads(open(recipe_file, "r").read())
+    
+    #make sure one of the output files is named as dataset name
+    flag = False
+    for obj in recipe_json["outputfiles"]:
+        if obj["filepath"].find(dataset) != -1:
+            flag = True
+    if flag == False:
+        print "None of the output file names is the same as the dataset (%s)" % (dataset)
+        sys.exit()
+
+
+
+    dump_log(log_file, "started making dataset", "write")
+
+    df_list = []
+    for obj in recipe_json["tablelist"]:
+        if obj["type"] != "input":
+            continue
+        data_frame = {}
+        sep = obj["separator"]
+        file_type = obj["filepath"].split(".")[-1]
+        if file_type in ["csv", "tsv"]:
+            if "sizecategory" in obj:
+                if obj["sizecategory"] in ["regular"]:
+                    csvutil.load_sheet(data_frame, obj["filepath"], obj["fields"], sep)
+                else:
+                    csvutil.load_large_sheet(data_frame, obj["filepath"], obj["fields"], sep)
+            else:
+                csvutil.load_large_sheet(data_frame, obj["filepath"], obj["fields"], sep)
+        elif file_type in ["json"]:
+            csvutil.load_sheet_from_json(data_frame, obj["filepath"], obj["fields"])
+        df_list.append(data_frame)
+
+    dump_log(log_file, "finished loading input", "append")
+
+
+    for obj in recipe_json["tablelist"]:
+        if obj["type"] != "output":
+            continue
+        if "action" in obj:
+            o = obj["action"]
+            if o["name"] == "union":
+                data_frame = csvutil.union_tables(df_list,obj["inputtables"])
+                df_list.append(data_frame)
+            elif o["name"] == "addconstantfields":
+                table_ind = obj["inputtables"][0] - 1
+                data_frame = csvutil.add_constant_fields(df_list,table_ind, o["newfields"])
+                df_list.append(data_frame)
+            elif o["name"] == "makecombofield":
+                table_ind = obj["inputtables"][0] - 1
+                data_frame = csvutil.add_combo_field(df_list,table_ind,o)
+                df_list.append(data_frame)
+            elif o["name"] == "filterin":
+                table_ind = obj["inputtables"][0] - 1
+                data_frame = csvutil.filter_in_records(df_list,table_ind,o)
+                df_list.append(data_frame)
+            elif o["name"] == "filterout":
+                table_ind = obj["inputtables"][0] - 1
+                data_frame = csvutil.filter_out_records(df_list,table_ind,o)
+                df_list.append(data_frame)
+            elif o["name"] == "expandrows":
+                table_ind = obj["inputtables"][0] - 1
+                data_frame = csvutil.expand_rows(df_list,table_ind,o)
+                df_list.append(data_frame)
+            elif o["name"] == "transposecols":
+                table_ind = obj["inputtables"][0] - 1
+                data_frame = csvutil.transpose_cols(df_list,table_ind,o)
+                df_list.append(data_frame)
+            elif o["name"] == "addnormalizedcol":
+                table_ind = obj["inputtables"][0] - 1
+                data_frame = csvutil.add_normalized_col(df_list,table_ind,o)
+                df_list.append(data_frame)
+            elif o["name"] == "addaveragecol":
+                table_ind = obj["inputtables"][0] - 1
+                data_frame = csvutil.add_average_col(df_list,table_ind,o)
+                df_list.append(data_frame)
+            elif o["name"] == "splitcol":
+                table_ind = obj["inputtables"][0] - 1
+                data_frame = csvutil.split_col(df_list,table_ind,o)
+                df_list.append(data_frame)
+            elif o["name"] == "jointables":
+                table_ind_one = obj["inputtables"][0] - 1
+                table_ind_two = obj["inputtables"][1] - 1
+                data_frame = csvutil.join_tables(df_list[table_ind_one], df_list[table_ind_two],
+                        o["anchorfields"])
+                df_list.append(data_frame)
+             
+
+
+
+    dump_log(log_file, "finished performing actions", "append")
+
+
+    for obj in recipe_json["outputfiles"]:
+        #populate id master list if masterlistfilterfield is set
+        masterlist_dict = {}
+        masterlist_field = ""
+        if "masterlistfilterfield" in obj:
+            if obj["masterlistfilterfield"] != "":
+                masterlist_field = obj["masterlistfilterfield"]
+                data_frame = {}
+                sep = "\",\""
+                fields = [masterlist_field]
+                csvutil.load_large_sheet(data_frame, masterlist_file, fields, sep)
+                for row in data_frame["data"]:
+                    masterlist_dict[row[0].upper()] = True
+        
+        table_ind = obj["tableid"] - 1
+        out_file = obj["filepath"]
+        data_frame = df_list[table_ind]
+
+        FW = open(out_file, "w")
+        newrow = []
+        field_map = {}
+        f_list_new = []
+        for o in obj["fields"]:
+            field_map[o["name"]] = o["newname"]
+            field_map[o["newname"]] = o["name"]
+            f_list_new.append(o["newname"])
+        FW.write("\"%s\"\n" % ("\",\"".join(f_list_new)))
+       
+        dump_log(log_file, "started processing output rows", "append")
+        row_count = 0
+        for row in data_frame["data"]:
+            row_count += 1
+            if row_count%100000 == 0:
+                dump_log(log_file, "processed %s output rows" % (row_count), "append")
+            
+            newrow = []
+            for f_new in f_list_new:
+                f_old = field_map[f_new]
+                value = row[data_frame["fields"].index(f_old)]
+                if f_new in ["log2fc"]:
+                    value = "%1.2f" %(float(value))
+                elif f_new in ["pvalue","adjpvalue"]:
+                    value = "%1.2e" %(float(value))
+                newrow.append(value)
+            
+            #filter out if masterlist_field is set
+            if masterlist_field != "":
+                masterlist_field_old = field_map[masterlist_field]
+                masterlist_value = row[data_frame["fields"].index(masterlist_field_old)].upper()
+                #print masterlist_field_old, masterlist_field, masterlist_value, masterlist_value in masterlist_dict 
+                if masterlist_value not in masterlist_dict:
+                    
+                    continue
+            FW.write("\"%s\"\n" % ("\",\"".join(newrow)))
+        FW.close()
+
+    dump_log(log_file, "finished making dataset", "append")
+
+    return
+
+
+
+
+
+###############################
+def main():
+
+    usage = "\n%prog  [options]"
+    parser = OptionParser(usage,version="%prog " + __version__)
+    parser.add_option("-d","--dataset",action="store",dest="dataset",help="[idmapping, transcriptlocus]")
+
+
+    (options,args) = parser.parse_args()
+    for file in ([options.dataset]):
+        if not (file):
+            parser.print_help()
+            sys.exit(0)
+
+    global config_obj
+    global species_obj
+
+    dataset = options.dataset
+
+    config_obj = json.loads(open("../conf/config.json", "r").read())
+    species_obj = config_obj["speciesinfo"]
+ 
+    global path_obj
+    path_obj = config_obj["pathinfo"]
+
+    
+    execute_recipe(dataset)
+    return
+
+
+
+
+
+
+if __name__ == '__main__':
+        main()
+
